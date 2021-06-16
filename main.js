5291ba8fb596ab6a17c37ca6debfc8fcac0a28e4
unknown <edgar.adirga@gmail.com>
2021-06-16 22:03:51 +0700

    vue

---
+++ main.js
@@ -0,0 +1,68 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Testing Vue',
+        backlog: [
+            {
+                id: 1,
+                name: 'first test',
+                description: '',
+                deadline: '17 juni'
+            },
+            {
+                id: 2,
+                name: 'second test',
+                description: '',
+                deadline: '17 juni'
+            }
+        ],
+        todo: [
+            {
+                id: 1,
+                name: 'first todo',
+                description: '',
+                deadline: '17 juni'
+            },
+            {
+                id: 2,
+                name: 'second todo',
+                description: '',
+                deadline: '17 juni'
+            } 
+        ],
+        doing: [
+            {
+                id: 1,
+                name: 'first doing',
+                description: '',
+                deadline: '17 juni'
+            },
+            {
+                id: 2,
+                name: 'second doing',
+                description: '',
+                deadline: '17 juni'
+            }
+        ],
+        done: [
+            {
+                id: 1,
+                name: 'first done',
+                description: '',
+                deadline: '17 juni'
+            },
+            {
+                id: 2,
+                name: 'second done',
+                description: '',
+                deadline: '17 juni'
+            }
+        ],
+        formNew: 0,
+        newName: "",
+        newDesc: "",
+        newDate: "",
+        newCat: "",
+    },
+})
+  

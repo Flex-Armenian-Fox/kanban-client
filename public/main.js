8e48cf7ee1ea6b59d2f5e8cc8a76d1e5c8bdda63
anggidast <adastariana@gmail.com>
2021-06-16 22:47:49 +0700

    CRUD client

--- public/main.js
+++ public/main.js
@@ -3,21 +3,49 @@ const app = new Vue({
   data: {
     message: 'I love Vue 3000',
     today: `${new Date().getDate()}-${new Date().toLocaleString('default', { month: 'short' })}`,
-    addCard: false,
+    addBacklog: false,
+    addTodo: false,
+    addDoing: false,
+    addDone: false,
     title: '',
-    tasks: [],
+    tasks: {
+      backlog: [],
+      todo: [],
+      doing: [],
+      done: [],
+    },
   },
   methods: {
     addTask(category) {
-      const task = {
-        title: this.title,
-        category,
-      };
-      this.tasks.push({
+      function capitalize(string) {
+        return 'add' + string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+      this.tasks[category].push({
         title: this.title,
         category,
       });
-      console.log(task);
+
+      this[capitalize(category)] = false;
+      this.title = '';
+      // console.log(task);
+    },
+    deleteTask(title, category) {
+      this.tasks[category] = this.tasks[category].filter((el) => el.title != title);
+    },
+    moveTask(title, category) {
+      let newCategory;
+      // console.log(title, category);
+      if (category == 'backlog') newCategory = 'todo';
+      else if (category == 'todo') newCategory = 'doing';
+      else if (category == 'doing') newCategory = 'done';
+      if (category != 'done') {
+        this.tasks[newCategory].push({
+          title,
+          category: newCategory,
+        });
+      }
+      this.deleteTask(title, category);
     },
   },
 });

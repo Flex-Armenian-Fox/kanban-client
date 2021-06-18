4e3d19971b0ee6cf72cfc362ae2ab7d39a00bdda
unknown <edgar.adirga@gmail.com>
2021-06-18 20:05:58 +0700

    putDate

--- main.js
+++ main.js
@@ -3,72 +3,76 @@ var app = new Vue({
     props: ['value', 'editing'],
     data: {
         title: 'Testing Vue',
-        backlog: [
-            {
-                id: 1,
-                name: 'first test',
-                description: '',
-                deadline: '17 juni'
-            },
-            {
-                id: 2,
-                name: 'second test',
-                description: '',
-                deadline: '17 juni'
-            }
-        ],
-        todo: [
-            {
-                id: 1,
-                name: 'first todo',
-                description: '',
-                deadline: '17 juni'
-            },
-            {
-                id: 2,
-                name: 'second todo',
-                description: '',
-                deadline: '17 juni'
-            } 
-        ],
-        doing: [
-            {
-                id: 1,
-                name: 'first doing',
-                description: '',
-                deadline: '17 juni'
-            },
-            {
-                id: 2,
-                name: 'second doing',
-                description: '',
-                deadline: '17 juni'
-            }
-        ],
-        done: [
-            {
-                id: 1,
-                name: 'first done',
-                description: '',
-                deadline: '17 juni'
-            },
-            {
-                id: 2,
-                name: 'second done',
-                description: '',
-                deadline: '17 juni'
-            }
-        ],
+        tasks: [],
         formNew: 0,
         newName: "",
-        newDesc: "",
         newDate: "",
         newCat: "",
+        targetTask: {
+            id: 0,
+            name: '',
+            deadline: '',
+        },
+        SERVER: "http://localhost:3000",
     },
     methods: {
         update(task, event){
             console.log(event.srcElement.text)
+        },
+        startDrag(task, event){
+            console.log(task.name)
+            this.targetTask = task
+            console.log(this.targetTask.id)
+        },
+        fetchData(){
+            axios.get(`${this.SERVER}/tasks`, {headers: {access_token: localStorage.access_token}})
+                .then(res => {
+                    console.log(res.data)
+                    res.data.forEach(el => {
+                        if (el.deadline){el.deadline = el.deadline.slice(0,10)}
+                        console.log(el.deadline)
+                    })
+                    this.tasks = res.data
+                })
+                .catch(err => {
+                    console.error(err); 
+                })
+        },
+        putDate(task, event){
+            axios.put(
+                `${this.SERVER}/tasks/${task.id}`, 
+                task, 
+                {headers: {access_token: localStorage.access_token}}
+            )
+            .then(res => {
+                console.log(res)
+                this.fetchData()
+            })
+            .catch(err => {
+                console.error(err); 
+            })
         }
+    },
+    computed: {        
+        backlog() {
+            return this.tasks.filter(task => task.category == 'backlog')
+        },
+        todo() {
+            return this.tasks.filter(task => task.category == 'todo')
+        },
+        doing() {
+            return this.tasks.filter(task => task.category == 'doing')
+        },
+        done() {
+            return this.tasks.filter(task => task.category == 'done')
+        },
+        deadline(deadline){
+            return deadline.toISOString().slice(0,10)
+        }
+    },
+    beforeMount() {
+        this.fetchData()
     }
+
 })
   

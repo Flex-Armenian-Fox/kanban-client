c990666f541bc4b22055d91e784cdddb6466b779
Karen <karen@KD-MM.local>
2021-06-20 22:31:59 +0700

    20 Jun - 22.31

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        gray: ['2px solid #D1D5DB', '2px'],
+      }
+    },
   },
   variants: {
     extend: {},
